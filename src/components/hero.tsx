4d26f80d1c26bbda7a1a65b4714331bf32e88138
asha <asha@ashas-MacBook-Air.local>
2026-05-03 23:33:12 -0500

    [feat]: new hero section

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,18 +1,21 @@
 export const Hero = () => {
   return (
     <section className="flex h-screen items-center justify-between gap-8">
-      <div className="flex flex-col">
+      <div className="flex w-2/3 flex-col border-r">
         <span className="text-6xl">Desarrollador</span>
         <span className="font-serif text-6xl italic">Diseñador</span>
         <span className="font-serif text-6xl italic">Web</span>
-      </div>
-      <div className="flex flex-col gap-3">
-        <p>
+        <p className="mt-4">
           <span className="pr-2 font-serif italic">Hola!</span>
           Mi nombre es{' '}
-          <span className="pr-2 font-serif italic">Javier Rojas</span>y soy
-          desarrollador web fullstack.
+          <span className="pr-2 font-serif italic">Javier Rojas.</span>
+          Desarrollador Web Full Stack y Productor de Medios. Construyendo
+          experiencias digitales meticulosas con un enfoque en diseño
+          estructural y rendimiento técnico impecable.
         </p>
+      </div>
+      <div className="flex flex-col gap-3">
+        <p>Contactame para trabajar juntos.</p>
       </div>
     </section>
   )
